a66df40cf6451ecc8ef4801e3aae8eb5798cf0e5
AndLego <galeon.gb@gmail.com>
2024-02-02 15:16:56 -0500

    basic layout for the right side of the work section

--- src/components/Works/Project/Project.tsx
+++ src/components/Works/Project/Project.tsx
@@ -3,17 +3,39 @@ import style from "./Project.module.css"
 import { Link } from 'react-router-dom';
 
 const Project = () => {
-    const links = ["https://i.postimg.cc/Kj9CyBWP/web-design-pic.jpg", "https://i.postimg.cc/9fJHtDPq/web-design-rest.jpg", "https://i.postimg.cc/667TqKwh/web-design-desk.jpg"]
-    const [currentImg, setCurrentImg] = React.useState(links[0])
+    const links = [
+        "https://i.postimg.cc/Kj9CyBWP/web-design-pic.jpg",
+        "https://i.postimg.cc/9fJHtDPq/web-design-rest.jpg",
+        "https://i.postimg.cc/667TqKwh/web-design-desk.jpg"]
+
+    const [currentLinkIndex, setCurrentLinkIndex] = React.useState(0);
+
+    React.useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentLinkIndex((prevIndex) => (prevIndex + 1) % links.length);
+        }, 4000);
+
+        return () => clearInterval(interval);
+    }, [links.length]);
+
     return (
         <article className={style.right}>
             <div className={style.imageContainer}>
-                <img src="https://i.postimg.cc/9fJHtDPq/web-design-rest.jpg" alt="" />
+                {links.map((link, index) => (
+                    <img
+                        key={index}
+                        src={link}
+                        alt=""
+                        className={index === currentLinkIndex ? '' : style.hidden}
+                    />
+                ))}
                 <div className={style.overlay}>
-                    <p className={style.description}>Como desarrollador Front End disfruto desde la meticulosa planificación hasta la resolución de desafíos, el proceso es una danza entre creatividad y soluciones técnicas. Cada problema resuelto es una pequeña victoria, y el resultado final es una obra maestra digital que conecta, inspira y transforma. Es más que código; es la alquimia de construir experiencias en línea significativas.</p>
-
+                </div>
+                <div className={style.content}>
+                    <h3>Código Encantador</h3>
+                    <p>Tejiendo magia digital, donde cada línea cuenta una historia visual </p>
                     <Link to="/projects" className={style.linkTo}>
-                        Go
+                        Vamos
                     </Link>
                 </div>
             </div>
